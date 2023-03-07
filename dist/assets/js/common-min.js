553bd2d2b688ac5024fd50c4a44a572cf770c3b0
quangminh1397 <minhtq@icd-vn.com>
2023-03-07 16:41:24 +0700

    fx

--- dist/assets/js/common-min.js
+++ dist/assets/js/common-min.js
@@ -8,5 +8,32 @@ $(document).ready(function () {
             })
         })
       }
+      $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        focusOnSelect: true
+      });
+
+      if ($('.m-blockdetail__tab ').length) {
+
+        $(".tab-ttl li").each(function(index){
+            $(this).click(function(){
+                $(".tab-ttl li").removeClass('active');
+                $(this).toggleClass('active');
+                $(".tab-cont .tab-frame").removeClass('active');
+                $(".tab-cont .tab-frame").eq(index).toggleClass('active');
+            })
+        })
+
+      }
     
 });
